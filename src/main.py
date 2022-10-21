5a439b59a19412fc9ac4f22f4e2b10c156950196
Shoichi Hasegawa <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2022-10-21 14:40:44 +0900

    fox word list

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# import sqlite3
+import sqlite3
 
 # WordNetデータの内容の確認
-## sqlite_masterという表から、nameという列のデータを取得
+# sqlite_masterという表から、nameという列のデータを取得
 # conn = sqlite3.connect("wnjpn.db")
 # cur = conn.execute("select name from sqlite_master where type='table'")
 # for row in cur:
@@ -29,13 +29,15 @@ import wordnet_jp
 import pprint
 
 ## 指定した単語の類義語、概念を取得
-word = ["plate", "bowl", "pitcher_base", "banana", "apple", "orange",
-        "cracker_box", "pudding_box", "chips_bag", "coffee", "muscat", "fruits_juice",
-        "pig_doll", "sheep_doll", "penguin_doll", "airplane_toy", "car_toy",
-        "truck_toy", "tooth_paste", "towel", "cup", "treatments", "sponge", "bath_slipper",
-        "frog_shaped_sponge", "duck_shaped_sponge"]
+# word = ["plate", "bowl", "pitcher_base", "banana", "apple", "orange",
+#         "cracker_box", "pudding_box", "chips_bag", "coffee", "muscat", "fruits_juice",
+#         "pig_doll", "sheep_doll", "penguin_doll", "airplane_toy", "car_toy",
+#         "truck_toy", "tooth_paste", "towel", "cup", "treatments", "sponge", "bath_slipper",
+#         "frog_shaped_sponge", "duck_shaped_sponge"]
 
 # word = 'penguin'
+
+word = ["living", "kitchen", "bathroom", "bedroom"]
 
 for w in range(len(word)):
     synonym = wordnet_jp.getSynonym(word[w])
